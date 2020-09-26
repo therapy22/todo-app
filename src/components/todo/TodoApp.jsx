7368fb46d692bebfcdaff4e30bf8e8459ad4c83e
therapy22 <suminjung09@gmail.com>
2020-09-26 18:46:49 +0900

    Adding Hardcoded Authentication to Login Component

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -17,14 +17,17 @@ class LoginComponent extends Component {
     this.state = {
       username: 'in28minutes',
       password: '',
+      hasLoginFailed: false,
+      showSuccessMessage: false,
     };
     /* this.handleUsernameChange = this.handleUsernameChange.bind(this);
     this.handlePasswordChange = this.handlePasswordChange.bind(this); */
     this.handleChange = this.handleChange.bind(this);
+    this.loginClicked = this.loginClicked.bind(this);
   }
 
   handleChange(event) {
-    console.log(event.target.name);
+    console.log(this.state);
     this.setState({
       [event.target.name]: event.target.value,
     });
@@ -44,9 +47,32 @@ class LoginComponent extends Component {
     });
   } */
 
+  loginClicked() {
+    if (
+      this.state.username === 'in28minutes' &&
+      this.state.password === 'dummy'
+    ) {
+      console.log('Successful');
+      this.setState({
+        showSuccessMessage: true,
+        hasLoginFailed: false,
+      });
+    } else {
+      console.log('Faild');
+      this.setState({
+        showSuccessMessage: false,
+        hasLoginFailed: true,
+      });
+    }
+  }
+
   render() {
     return (
       <div>
+        <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
+        <ShowLoginSuccessful
+          showSuccessMessage={this.state.showSuccessMessage}
+        />
         User Name:{''}
         <input
           type='text'
@@ -61,10 +87,24 @@ class LoginComponent extends Component {
           value={this.state.password}
           onChange={this.handleChange}
         />
-        <button>Login</button>
+        <button onClick={this.loginClicked}>Login</button>
       </div>
     );
   }
+}
+
+function ShowInvalidCredentials(props) {
+  if (props.hasLoginFailed) {
+    return <div>Invalid Credentials</div>;
+  }
+  return null;
+}
+
+function ShowLoginSuccessful(props) {
+  if (props.showSuccessMessage) {
+    return <div>Login Sucessful</div>;
+  }
+  return null;
 }
 
 export default TodoApp;
