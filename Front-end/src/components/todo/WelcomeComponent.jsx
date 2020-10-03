dfa9f1223413c792198aae1bde3a54c939112f3f
therapy22 <suminjung09@gmail.com>
2020-10-03 11:23:57 +0900

    Allow Cross Origin Requests

--- Front-end/src/components/todo/WelcomeComponent.jsx
+++ Front-end/src/components/todo/WelcomeComponent.jsx
@@ -6,17 +6,21 @@ class WelcomeComponent extends Component {
   constructor(props) {
     super(props);
     this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+    this.handleSuccessfulResponse = this.handleSuccessfulResponse.bind(this);
+    this.state = {
+      welcomeMessage: '',
+    };
   }
 
   render() {
     return (
       <>
         <h1>Welcome!</h1>
-        <div>
+        <div className='container'>
           Welcome {this.props.match.params.name}. You can manage your todos
           <Link to='/todos'> here</Link>.
         </div>
-        <div>
+        <div className='container'>
           Click here to get a customized welcome message.
           <button
             onClick={this.retrieveWelcomeMessage}
@@ -25,15 +29,22 @@ class WelcomeComponent extends Component {
             Get Welcome Message
           </button>
         </div>
+        <div className='container'>{this.state.welcomeMessage}</div>
       </>
     );
   }
 
   retrieveWelcomeMessage() {
     HelloWorldService.executeHelloWorldService().then((response) =>
-      console.log(response)
+      this.handleSuccessfulResponse(response)
     );
     //.catch()
+  }
+
+  handleSuccessfulResponse(response) {
+    this.setState({
+      welcomeMessage: response.data,
+    });
   }
 }
 
