38ab79bf7fbec48ede9fc6354d4ac21e6a7ab408
therapy22 <suminjung09@gmail.com>
2020-10-05 21:19:01 +0900

    Created DELETE Request

--- Back-end/restful-web-services/src/main/java/com/suminjung/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ Back-end/restful-web-services/src/main/java/com/suminjung/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -3,7 +3,9 @@ package com.suminjung.rest.webservices.restfulwebservices.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +21,16 @@ public class TodoResource {
 	@GetMapping("/users/{username}/todos")
 	public List<Todo> getAllTodos(@PathVariable String username) {
 		return todoService.findAll();
-}
+	}
+		
+	@DeleteMapping("/users/{username}/todos/{id}")
+	public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
+		Todo todo = todoService.deleteById(id);
+		
+		if(todo!=null) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		return ResponseEntity.notFound().build();
+	}
 }
